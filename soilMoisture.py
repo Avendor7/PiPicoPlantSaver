a27914eecdef4e56dd41b33e50caefb6b590256c
StephenW <steve.wiggins7@gmail.com>
2025-03-23 11:05:21 -0400

    Remove example LCD code

--- soilMoisture.py
+++ soilMoisture.py
@@ -1,20 +1,14 @@
 import machine
 import utime
-import I2C_LCD_driver
 
-led_onboard = machine.Pin(25, machine.Pin.OUT)
 adc = machine.ADC(26)
-lcd = I2C_LCD_driver.lcd()
 conversion_factor = 100 / (65535)
 
 while True:
     moisture = 130 - (adc.read_u16() * conversion_factor)
     print("Moisture: ", round(moisture, 1), "% - ", utime.localtime())
-    lcd.lcd_display_string("Moisture: {:.1f}%  ".format(moisture,), 1)
     
     if moisture >= 70 :
-        led_onboard.value(0)
-        utime.sleep(30)
+        utime.sleep_ms(500)
     elif moisture < 70 :
-        led_onboard.toggle()
         utime.sleep_ms(500)
