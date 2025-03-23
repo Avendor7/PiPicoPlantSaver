0a6d164f3383b8b7f8ee2062cd6c99cb0ed691d8
StephenW <steve.wiggins7@gmail.com>
2025-03-23 12:20:09 -0400

    fix main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from machine import Pin, I2C
-import utime as time
-from dht import DHT11, InvalidChecksum
+from machine import Pin
+from time import sleep
+import dht 
+
+#sensor = dht.DHT22(Pin(22))
+sensor = dht.DHT11(Pin(28))
 
 while True:
-    time.sleep(5)
-    pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
-    sensor = DHT11(pin)
-    t  = (sensor.temperature)
-    h = (sensor.humidity)
-    print("Temperature: {}".format(sensor.temperature))
-    print("Humidity: {}".format(sensor.humidity))
+  try:
+    sleep(5)
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    print('Temperature: %3.1f C' %temp)
+    print('Humidity: %3.1f %%' %hum)
+  except OSError as e:
+    print('Failed to read sensor.')
